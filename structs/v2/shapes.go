63a0f0f791631c56dd34508f4fa299feacb895dd
Godson Sakawa <gody@pc.>
2025-01-08 17:40:47 +0300

    simple area implementation

---
+++ structs/v2/shapes.go
@@ -0,0 +1,11 @@
+package main
+
+// Area returns the area of a rectangle.
+
+func Area (widht, height float64) float64{
+	return (widht * height)
+}
+// Perimeter returns the perimeter of a rectangle.
+func Perimeter(width float64, height float64) float64{
+	return (width + height) * 2
+}
