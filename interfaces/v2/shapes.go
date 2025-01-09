d4d4eca8819a6d77b45614a33692ca4d6527d432
Godson Sakawa <gody@pc.>
2025-01-09 19:55:09 +0300

    Table driven tests Intro

---
+++ interfaces/v2/shapes.go
@@ -0,0 +1,34 @@
+package main
+
+import "math"
+
+// Shape is an interface that defines the Area method.
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle has the dimensions of a rectangle.
+type Rectangle struct {
+	width float64
+	height float64
+}
+
+// Circle represents a circle
+type Circle struct {
+	radius float64
+}
+
+// Area returns the area of a rectangle.
+func (r Rectangle) Area() float64{
+	return (r.width * r.height)
+}
+
+// Perimeter returns the perimeter of a rectangle.
+func Perimeter(rectangle Rectangle) float64{
+	return (rectangle.width + rectangle.height) * 2
+}
+
+// Area returns the area of a cirle 
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
